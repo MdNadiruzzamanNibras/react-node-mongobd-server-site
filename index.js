a850967caaef3adba8daae2eebfa9fc8aa75eb14
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-25 11:40:52 +0600

    app get and post

--- index.js
+++ index.js
@@ -15,10 +15,22 @@ async function run(){
   try{
     await client.connect()
     const userCollection = client.db("TheCollection").collection("user")
-    const user ={name:'mahi', email:'mahi@gmail.com'}
-    const result =await userCollection.insertOne(user)
-    console.log(`user insterted with id:${result.insertedId}`)
+    // const user ={name:'mahi', email:'mahi@gmail.com'}
+    // const result =await userCollection.insertOne(user)
+    // console.log(`user insterted with id:${result.insertedId}`)
+    app.get('/user', async(req,res)=>{
+      const query ={}
+      const cursor =userCollection.find(query);
+      const users = await cursor.toArray()
+      res.send(users)
+    })
 
+    app.post('/user', async(req,res)=>{
+      const newUser= req.body;
+      console.log("collecd te user", newUser)
+      const result = await userCollection.insertOne(newUser)
+      res.send(result)
+    })
   }
   finally{
     // await client.close
