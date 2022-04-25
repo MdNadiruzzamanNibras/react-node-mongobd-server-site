9cd155624095834abf055bd899bf0ae547296c36
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-25 16:08:33 +0600

    deleted user

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express= require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const { request } = require('express');
+const { ObjectID } = require('bson');
 const port =process.env.PORT ||5000;
-
+const ObjectId = require('mongodb').ObjectId
 // user:dbuser1
 // password:GSDQ27sgiU4PELAX
 
@@ -29,6 +31,15 @@ async function run(){
       const newUser= req.body;
       console.log("collecd te user", newUser)
       const result = await userCollection.insertOne(newUser)
+      res.send(result)
+
+      
+    })
+    app.delete('/user/:id', async(req,res)=>{
+      const id = req.params.id
+      console.log(id)
+      const query = {_id: ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
       res.send(result)
     })
   }
